54d8c169b786764da79922f4dd23ac4c19399cdd
ran <randyparadita31@gmail.com>
2025-04-08 23:11:17 +0800

    soal3 ujian done

---
+++ quiz/ujian/soal3.js
@@ -0,0 +1,33 @@
+/*
+diberikan sebuah function groupAnimals(animals) yang menerima satu parameter berupa array,
+fungsi ini akan me-return array 2 dimensi
+*/
+function groupAnimals(animals) {
+    // you can only write your code here!
+    grouppedAnimals = [];
+    usedIndice = [];
+    
+    for (let i = 0; i < animals.length; i++) {
+        if(!usedIndice.includes(i)) {
+            usedIndice.push(i);
+            group = [animals[i]];
+            firstLetter = group[0][0];
+
+            for(let i2 = i + 1; i2 < animals.length; i2++){
+                if(animals[i2][0] === firstLetter) {
+                    group.push(animals[i2]);
+                    usedIndice.push(i2);
+                }
+            }
+            
+            grouppedAnimals.push(group)
+        }
+    }
+    return grouppedAnimals.sort();
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
